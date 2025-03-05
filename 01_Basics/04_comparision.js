19b181b75f7cf03c2765bbf8434bbc14b17b6fe0
Pawan A. B. <pawanborkarmail@gmail.com>
2025-03-05 06:39:49 +0000

    compare

--- 01_Basics/04_comparision.js
+++ 01_Basics/04_comparision.js
@@ -11,10 +11,11 @@
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
+console.log(null < 0);
 
 console.log(undefined == 0);
 console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined < 0); 
 console.log(undefined >= 0);
 console.log(undefined <= 0);
 
